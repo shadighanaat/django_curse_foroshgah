08e8ace3165afb0c0552d406acaae0e08c59a5f2
shadighanaat/foroshgah <shadighanaati@gmail.com>
2024-05-13 08:33:11 +0330

    Doing Magritte's operation

--- config/settings.py
+++ config/settings.py
@@ -130,3 +130,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# accounts config
+AUTH_USER_MODEL = 'accounts.CustomUser'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
