a5595817059af477e1eab0c7204fcd83f3b0ba66
shadighanaat/foroshgah <shadighanaati@gmail.com>
2024-05-25 15:41:11 +0330

    add folder static to settings and url

--- config/urls.py
+++ config/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -24,5 +26,4 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     # rosseta (i18)
     path('rosetta/', include('rosetta.urls')),
-    
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
