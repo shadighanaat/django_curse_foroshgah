a8f579ea9eddc23a8a37b39cf53e78bed6f60ea3
shadighanaat/foroshgah <shadighanaati@gmail.com>
2024-05-27 20:37:58 +0330

    add url men and women at products

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import ProductListView, ProductDetailView, CommentCreateView
+from .views import ProductListView, ProductDetailView, ProductListMenView,  ProductListFeminineView, CommentCreateView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
+    path('men/', ProductListMenView.as_view(), name='product_list_men'),
+    path('feminine/', ProductListFeminineView.as_view(), name='product_list_feminine'),
     path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('comment/<int:product_id>/', CommentCreateView.as_view(), name='comment_create'),
 ]
