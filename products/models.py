b62ed7d6d093f818ea19eb535588dd1ab1e2ae0e
shadighanaat/foroshgah <shadighanaati@gmail.com>
2024-05-29 00:40:33 +0330

    making products for pages at models

--- products/models.py
+++ products/models.py
@@ -37,10 +37,183 @@ class Product(models.Model):
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.pk])
 
+class ActiveCommentsManager(models.Manager):
+    def get_queryset(self):
+        return super(ActiveCommentsManager, self).get_queryset().filter(active=True)
+    
+class ProductMen(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    short_description = models.TextField(blank=True)
+    price = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+    image = models.ImageField(_('Product Image'), upload_to='product/product_cover/', blank=True, )
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.pk])  
+
+class ProductFeminine(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    short_description = models.TextField(blank=True)
+    price = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+    image = models.ImageField(_('Product Image'), upload_to='product/product_cover/', blank=True, )
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.pk])  
+              
+class ProductChildish(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    short_description = models.TextField(blank=True)
+    price = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+    image = models.ImageField(_('Product Image'), upload_to='product/product_cover/', blank=True, )
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.pk])  
+    
+class ProductRefriGerator(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    short_description = models.TextField(blank=True)
+    price = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+    image = models.ImageField(_('Product Image'), upload_to='product/product_cover/', blank=True, )
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.pk])  
+              
+class ProductWashing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    short_description = models.TextField(blank=True)
+    price = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+    image = models.ImageField(_('Product Image'), upload_to='product/product_cover/', blank=True, )
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.pk])
+
+class ProductCooking(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    short_description = models.TextField(blank=True)
+    price = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+    image = models.ImageField(_('Product Image'), upload_to='product/product_cover/', blank=True, )
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.pk])                           
+class ProductLaptop(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    short_description = models.TextField(blank=True)
+    price = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+    image = models.ImageField(_('Product Image'), upload_to='product/product_cover/', blank=True, )
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.pk])   
+
+class ProductHeadphone(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    short_description = models.TextField(blank=True)
+    price = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+    image = models.ImageField(_('Product Image'), upload_to='product/product_cover/', blank=True, )
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.pk])      
+
+class ProductOffice(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    short_description = models.TextField(blank=True)
+    price = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+    image = models.ImageField(_('Product Image'), upload_to='product/product_cover/', blank=True, )
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.pk])  
+    
+class ProductList(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    short_description = models.TextField(blank=True)
+    price = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+    image = models.ImageField(_('Product Image'), upload_to='product/product_cover/', blank=True, )
+
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('product_detail', args=[self.pk])      
 
 class ActiveCommentsManager(models.Manager):
     def get_queryset(self):
         return super(ActiveCommentsManager, self).get_queryset().filter(active=True)
+
 
 
 class Comment(models.Model):
@@ -52,6 +225,16 @@ class Comment(models.Model):
         ('5', _('Very Good')),
     ]
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments', )
+    productlist = models.ForeignKey(ProductList, on_delete=models.CASCADE, related_name='comments' , null=True, )
+    productmen = models.ForeignKey(ProductMen, on_delete=models.CASCADE, related_name='comments',  null=True, )
+    productfeminine = models.ForeignKey(ProductFeminine, on_delete=models.CASCADE, related_name='comments', null=True, )
+    productchildish = models.ForeignKey(ProductChildish, on_delete=models.CASCADE, related_name='comments', null=True, )
+    productrefrigerator = models.ForeignKey(ProductRefriGerator, on_delete=models.CASCADE, related_name='comments', null=True, )
+    productWashing = models.ForeignKey(ProductWashing, on_delete=models.CASCADE, related_name='comments', null=True, )
+    productcooking = models.ForeignKey(ProductCooking, on_delete=models.CASCADE, related_name='comments', null=True, )
+    productlaptop = models.ForeignKey(ProductLaptop, on_delete=models.CASCADE, related_name='comments', null=True, )
+    productheadphone = models.ForeignKey(ProductHeadphone, on_delete=models.CASCADE, related_name='comments', null=True, )
+    productoffice = models.ForeignKey(ProductOffice, on_delete=models.CASCADE, related_name='comments', null=True, )
     author = models.ForeignKey(get_user_model(),
                                on_delete=models.CASCADE,
                                related_name='comments',
@@ -69,3 +252,4 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return reverse('product_detail', args=[self.product.id])
+
