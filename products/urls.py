b8bbbae0c6803d1ed9ec3733898e44844eae1257
shadighanaat/foroshgah <shadighanaati@gmail.com>
2024-05-30 12:23:15 +0330

    add urls at app products

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,30 @@
 from django.urls import path
 
-from .views import ProductListView, ProductDetailView, ProductListMenView,  ProductListFeminineView, CommentCreateView
+from .views import (ProductListView, 
+                    ProductListMenView,
+                    ProductListFeminineView,
+                    ProductListChildishView,
+                    ProductListRefrigeratorView,
+                    ProductListWashingView,
+                    ProductListCookingView,
+                    ProductListLaptopView,
+                    ProductListHeadphoneView,
+                    ProductListOfficeView,
+                    ProductDetailView,
+                    CommentCreateView,
+)
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('men/', ProductListMenView.as_view(), name='product_list_men'),
     path('feminine/', ProductListFeminineView.as_view(), name='product_list_feminine'),
+    path('childish/', ProductListChildishView.as_view(), name='product_list_childish'),
+    path('refrigerator/', ProductListRefrigeratorView.as_view(), name='product_list_refrigerator'),
+    path('washing/', ProductListWashingView.as_view(), name='product_list_washing'),
+    path('cooking/', ProductListCookingView.as_view(), name='product_list_cooking'),
+    path('laptop/', ProductListLaptopView.as_view(), name='product_list_laptop'),
+    path('headphone/', ProductListHeadphoneView.as_view(), name='product_list_headphone'),
+    path('office/', ProductListOfficeView.as_view(), name='product_list_office'),
     path('<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('comment/<int:product_id>/', CommentCreateView.as_view(), name='comment_create'),
 ]
