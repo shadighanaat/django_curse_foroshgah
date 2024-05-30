3b1a7a887e06d12d62b98679eb10e5c9cb9d9318
shadighanaat/foroshgah <shadighanaati@gmail.com>
2024-05-30 12:08:48 +0330

    making admin at products

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Product, Comment, Category
+from .models import (Product, Comment, Category, ProductMen, 
+                      ProductFeminine,
+                      ProductChildish, 
+                      ProductRefriGerator, 
+                      ProductWashing, 
+                      ProductLaptop, 
+                      ProductHeadphone, 
+                      ProductOffice, 
+                      
+)
 
 
 class CommentInline(admin.TabularInline):
@@ -11,6 +20,8 @@ class CommentInline(admin.TabularInline):
 class CategoryAdmin(admin.ModelAdmin):
      list_display = ['title']
 
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['title', 'price', 'active']
@@ -18,7 +29,65 @@ class ProductAdmin(admin.ModelAdmin):
         CommentInline
     ]
 
+@admin.register(ProductMen)
+class ProductMenAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'active']
+    inlines = [
+        CommentInline
+    ]
+
+@admin.register(ProductFeminine)
+class ProductFeminineAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'active']
+    inlines = [
+        CommentInline
+    ]    
+
+@admin.register(ProductChildish)
+class ProductChildishAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'active']
+    inlines = [
+        CommentInline
+    ]            
+
+@admin.register(ProductRefriGerator)
+class ProductRefrigeatorAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'active']
+    inlines = [
+        CommentInline
+    ]    
+
+
+@admin.register(ProductWashing)
+class ProductFeminineAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'active']
+    inlines = [
+        CommentInline
+    ]    
+
+@admin.register(ProductLaptop)
+class ProductLaptopAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'active']
+    inlines = [
+        CommentInline
+    ]    
+
+@admin.register(ProductHeadphone)
+class ProductHeadphoneAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'active']
+    inlines = [
+        CommentInline
+    ]        
+
+@admin.register(ProductOffice)
+class ProductOfficeAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'active']
+    inlines = [
+        CommentInline
+    ]     
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['product', 'body', 'author', 'stars', 'active']
+
+
