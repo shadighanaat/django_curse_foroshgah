51011474383687b02f16253d8483a9fcca3488e8
shadighanaat/foroshgah <shadighanaati@gmail.com>
2024-06-24 01:08:36 +0330

    Do the straightening

--- orders/admin.py
+++ orders/admin.py
@@ -7,7 +7,20 @@ from .models import Order, OrderItem
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
-    fields = ['order', 'product', 'quantity', 'price', ]
+    fields =  ['order',  
+                    'product',  
+                    'productmen',
+                    'productfeminine',
+                    'productchildish',
+                    'productrefriGerator',
+                    'productwashing',
+                    'productcooking',
+                    'productlaptop',
+                    'productheadphone',
+                    'productoffice',
+                    'quantity', 
+                    'price', 
+                    ]
     extra = 1
 
 
@@ -22,5 +35,18 @@ class OrderAdmin(admin.ModelAdmin):
 
 @admin.register(OrderItem)
 class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ['order', 'product', 'quantity', 'price', ]
+    list_display = ['order',  
+                    'product',  
+                    'productmen',
+                    'productfeminine',
+                    'productchildish',
+                    'productrefriGerator',
+                    'productwashing',
+                    'productcooking',
+                    'productlaptop',
+                    'productheadphone',
+                    'productoffice',
+                    'quantity', 
+                    'price', 
+                    ]
 
